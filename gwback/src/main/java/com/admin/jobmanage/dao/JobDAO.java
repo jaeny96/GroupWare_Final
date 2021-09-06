04b4dc6114f0e24c6e9c4941c7edcf1d4a93f5ad
bmbkje <wjddms@ajou.ac.kr>
2021-09-06 21:56:07 +0900

    직무관리 최종 수정
    전달값 VO에서 Map형식으로 변경,
    업데이트시에도 한프로세스로 처리하게 변경
    모달창 넓이 변경

--- gwback/src/main/java/com/admin/jobmanage/dao/JobDAO.java
+++ gwback/src/main/java/com/admin/jobmanage/dao/JobDAO.java
@@ -1,6 +1,7 @@
 package com.admin.jobmanage.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import com.group.employee.dto.Employee;
 import com.group.employee.dto.Job;
@@ -25,8 +26,15 @@ public interface JobDAO {
 	 * @throws AddException
 	 * @param List<Job>
 	 */
-	public void selectAndDeleteAndInsert(List<Job>jobs) throws AddException;
-
+	public void selectAndDeleteAndInsert(List<Map<String, String>> jobList) throws AddException;
+	/**(저장버튼 관련)+update
+	 * 직무를 추가하기 전에 
+	 * 모든 직무를 검색후 모든 직무 삭제한다. 그 다음 새로운 직무들을 추가한다 
+	 * @throws AddException
+	 * @param List<Job>
+	 */
+	public void selectAndDeleteAndInsert(String oldJobId,List<Map<String,String>> jobList,
+			List<Map<String, String>> employeeList) throws ModifyException;
 	/**(x버튼 관련)
 	 * 해당 직무를 가지고 있는 사원들의 이름을 가지고온다. 
 	 * @param jobId
@@ -34,12 +42,7 @@ public interface JobDAO {
 	 * @exception FindException
 	 */
 	public List<Employee> selectEmpName(String jobId) throws FindException, FindException ;
-	
-	/**(x모달 관련)
-	 * 받아온 사원의 직무를 변경한다. 
-	 * @param oldJobId 변경전 직무,newJobId 변경후 직무,name 변경할 사원 
-	 * @exception RemoveException
-	 */
-	public void updateJobEep(String oldJobId,List<Employee> employees) throws ModifyException;
+
+
 
 }
